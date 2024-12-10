9fd96f9123758706933bff451b656ce127639556
Reeflex-M <123174183+Reeflex-M@users.noreply.github.com>
2024-12-10 11:05:27 +0100

    legalnotice

---
+++ src/pages/LogalNotice/LegalNotice.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LegalNotice = () => {
+    return (
+        <div>
+            <h1>Mentions Légales</h1>
+            <h2>Propriétaire du site</h2>
+            <p>Association les Mistoufles – Rue du Grand Launay – 29600 Saint-Martin-des-Champs</p>
+            <h2>Responsable de publication</h2>
+            <p>Camille FLOCH, bénévole</p>
+            <h2>Réalisation du site</h2>
+            <p>Camille FLOCH, bénévole et Perrine CADIET, à titre bénévole</p>
+            <h2>Hébergement</h2>
+            <p>GANDI SAS<br />
+            Société par Actions Simplifiée au capital de 37.000€ ayant son siège social au 15 place de la Nation à Paris (75011) FRANCE<br />
+            Immatriculée sous le numéro 423 093 459 RCS PARIS<br />
+            N° TVA FR81423093459<br />
+            Téléphone : +33.(1) 70.37.76.61<br />
+            Site web : <a href='https://www.gandi.net'>https://www.gandi.net</a></p>
+            <h2>Crédits</h2>
+            <p>Pour les photos du site : Association les Mistoufles ou Canva Pro</p>
+            <h2>Contenu du site</h2>
+            <p>Les informations disponibles dans ce site sont purement informatives. Le site est régulièrement mis à jour. Cependant, aucune garantie n’est apportée concernant l’exactitude, la précision, la fréquence de mise à jour des informations publiées sur ce site. L’Association les Mistoufles décline donc toute responsabilité pour d’éventuels litiges résultant notamment d’une imprécision ou inexactitude des informations disponibles sur son site.</p>
+            <h2>Droits d’auteur</h2>
+            <p>Tous droits réservés. Les textes, images, illustrations et autres médias sont soumis à la protection du droit d’auteur. Toute reproduction, modification, transmission du contenu de ce site pour une publication écrite ou électronique est strictement interdite sauf autorisation préalable du responsable de publication.</p>
+            <h2>Données collectées</h2>
+            <p>Lors de vos envois de formulaires, les données sont collectées par l’Association les Mistoufles pendant une durée limitée. Ces informations resteront strictement confidentielles et ne seront pas communiquées à des tiers. La loi « Informatique et Libertés » du 6 janvier 1978 modifiée par la loi du 6 août 2004 encadre la mise en œuvre des fichiers ou des traitements de données à caractère personnel qu’ils soient automatisés ou manuels. Vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent (art. 34 de la loi « Informatique et Libertés »). Vous pouvez l’exercer par courrier en écrivant à : Association les Mistoufles, Rue du Grand Launay, 29600 Saint-Martin-des-Champs. Vous pouvez également exercer votre droit d’accès, de modification, de rectification et suppression des données via une demande par e-mail : associationlesmistoufles@laposte.net</p>
+            <p>Le présent site web collecte des données personnelles via des formulaires de saisie uniquement dans une but d’information et de communication. Conformément à l’ordonnance n° 2018-1125 du 12 décembre 2018, publiée le 13 décembre 2018, il n’a pas fait l’objet d’une déclaration spécifique auprès de la CNIL.</p>
+        </div>
+    );
+};
+
+export default LegalNotice;
